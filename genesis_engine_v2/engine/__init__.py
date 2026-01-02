d210b6a10213ee7490503e22ac7716eb13c6d0ae
AnuzkaSharma <anuzka.care@gmail.com>
2026-01-02 21:07:29 +0530

    successfully implemented 3.1: Simple External Fitness Evaluator

--- genesis_engine_v2/engine/__init__.py
+++ genesis_engine_v2/engine/__init__.py
@@ -11,6 +11,8 @@ from .kernel_world import KernelWorld
 from .evolvable_genome import EvolvableGenome
 from .linkage_structure import LinkageStructure
 from .structurally_evolvable_agent import StructurallyEvolvableAgent
+from .bootstrap_evaluator import calculate_fitness, tournament_selection
 
 __all__ = ['CodonTranslator', 'ArtificialImmuneSystem', 'KernelAgent', 'KernelWorld', 
-           'EvolvableGenome', 'LinkageStructure', 'StructurallyEvolvableAgent']
+           'EvolvableGenome', 'LinkageStructure', 'StructurallyEvolvableAgent',
+           'calculate_fitness', 'tournament_selection']
